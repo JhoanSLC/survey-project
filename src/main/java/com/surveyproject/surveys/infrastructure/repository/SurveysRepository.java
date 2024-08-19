592fa7e4657af0cc97ef72553a58c5eb92830e3a
JhoanSLC <JhoanSLC4@gmail.com>
2024-08-19 12:36:58 -0500

    :construction: Surveys list all finished :construction:

--- src/main/java/com/surveyproject/surveys/infrastructure/repository/SurveysRepository.java
+++ src/main/java/com/surveyproject/surveys/infrastructure/repository/SurveysRepository.java
@@ -5,16 +5,21 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import com.surveyproject.database.DatabaseConnection;
+import com.surveyproject.screen.ScreenController;
 import com.surveyproject.surveys.domain.entity.Surveys;
 import com.surveyproject.surveys.domain.service.SurveysService;
 
 public class SurveysRepository implements SurveysService{
     private final DatabaseConnection database = new DatabaseConnection();
     private final Connection con = database.connectDatabase();
+    
+    private final ScreenController scr = new ScreenController();
+
     @Override
     public void createSurveys(Surveys survey) {
         String createQuery = "INSERT INTO surveys(created_at,updated_at,description,name) values (?,?,?,?)";
@@ -48,11 +53,15 @@ public class SurveysRepository implements SurveysService{
                     return Optional.of(resultSurvey);
                 }
             } catch (Exception e) {
+                scr.clean();
                 System.out.println("Error at result set line 41 - SurveysRepository");
                 e.printStackTrace();
+                scr.pause();
             }
         } catch (Exception e) {
+            scr.clean();
             e.printStackTrace();
+            scr.pause();
         }
         return Optional.empty();
         
@@ -60,8 +69,22 @@ public class SurveysRepository implements SurveysService{
 
     @Override
     public List<Surveys> listAllSurveys() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'listAllSurveys'");
+        String listAllQuery = "SELECT id,created_at,updated_at,description,name FROM surveys";
+        List<Surveys> resultSurveys = new ArrayList<>();
+
+        try (PreparedStatement ps = con.prepareStatement(listAllQuery); ResultSet rs = ps.executeQuery()){
+            while (rs.next()){
+                Surveys survey = new Surveys();
+                survey.setCreated_at(rs.getTimestamp("created_at").toLocalDateTime());
+                survey.setUpdated_at(rs.getTimestamp("updated_at").toLocalDateTime());
+                survey.setDescription(rs.getString("description"));
+                survey.setName(rs.getString("name"));
+                resultSurveys.add(survey);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resultSurveys;
     }
 
     @Override
