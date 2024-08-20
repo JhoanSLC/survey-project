0d9d9f1f6ae65b65fa4796e034469b29874e5f8a
JhoanSLC <JhoanSLC4@gmail.com>
2024-08-20 11:03:23 -0500

    :sparkles: CategoriesCatalog entity :sparkles:

---
+++ src/main/java/categories_catalog/domain/entity/CategoriesCatalog.java
@@ -0,0 +1,47 @@
+package categories_catalog.domain.entity;
+
+import java.time.LocalDateTime;
+
+public class CategoriesCatalog {
+    private long id;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String name;
+
+    public CategoriesCatalog(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+
+}
