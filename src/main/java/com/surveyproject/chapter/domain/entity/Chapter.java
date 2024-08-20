fb7c40d62beb8d34af0f490ea4150d5f511e51d6
JhoanSLC <JhoanSLC4@gmail.com>
2024-08-20 11:49:26 -0500

    :sparkles: Chapter entity :sparkles:

---
+++ src/main/java/com/surveyproject/chapter/domain/entity/Chapter.java
@@ -0,0 +1,64 @@
+package com.surveyproject.chapter.domain.entity;
+
+import java.time.LocalDateTime;
+
+public class Chapter {
+    private long id;
+    private LocalDateTime createdAt;
+    private long surveyId;
+    private LocalDateTime updatedAt;
+    private String chapterNumber;
+    private String chapterTitle;
+
+    public Chapter(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public long getSurveyId() {
+        return surveyId;
+    }
+
+    public void setSurveyId(long surveyId) {
+        this.surveyId = surveyId;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getChapterNumber() {
+        return chapterNumber;
+    }
+
+    public void setChapterNumber(String chapterNumber) {
+        this.chapterNumber = chapterNumber;
+    }
+
+    public String getChapterTitle() {
+        return chapterTitle;
+    }
+
+    public void setChapterTitle(String chapterTitle) {
+        this.chapterTitle = chapterTitle;
+    }
+
+    
+}
