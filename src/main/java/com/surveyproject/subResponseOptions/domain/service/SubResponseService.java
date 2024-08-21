f137ae8abf9089702c45923fed86bd3be32c4a0e
JhoanSLC <JhoanSLC4@gmail.com>
2024-08-21 07:16:51 -0500

    :sparkles: Sub response service :sparkles:

---
+++ src/main/java/com/surveyproject/subResponseOptions/domain/service/SubResponseService.java
@@ -0,0 +1,14 @@
+package com.surveyproject.subResponseOptions.domain.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.surveyproject.subResponseOptions.domain.entity.SubResponseOptions;
+
+public interface SubResponseService {
+    void createSubResponse(SubResponseOptions subResponse);
+    Optional<SubResponseOptions> findSubResponseById(long id);
+    List<SubResponseOptions> listAllSubResponses();
+    void updateSubResponse(SubResponseOptions subResponse);
+    void deleteSubResponse(long id);
+}
